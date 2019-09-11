431d6e9370318525ca985e91f4a640da6ad2785a
endri1987 <berberi.endri@gmail.com>
2019-09-11 16:00:56 +0200

    vetem foldera

--- ajxDt.php
+++
@@ -1,5 +0,0 @@
-<?php
-define("BO_ENVIRONMENT", "AJAX_RESP");
-
-INCLUDE_ONCE "application.php";
-INCLUDE_once EASY_PATH."mc/php/ajax_mc.php";
--- show_sig_logo.php
+++
@@ -1,4 +0,0 @@
-<?php
-INCLUDE_ONCE dirname(__FILE__)."/application.php";
-INCLUDE EASY_PATH."/mc/php_asp/show_sig_logo.php";
-?>
